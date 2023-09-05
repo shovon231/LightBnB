f3dac9413e7f0ece5cce37b281391024059fcf2a
shovon231 <shovon231@gmail.com>
2023-09-05 18:14:02 -0400

    adding full outer join in the getallproperties function and finalizing the project

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -105,7 +105,7 @@ const getAllProperties = (options, limit = 10) => {
   let queryString = `
     SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
-    JOIN property_reviews ON properties.id = property_id 
+    FULL OUTER JOIN property_reviews ON properties.id = property_id 
     WHERE 1=1
   `;
   if (options.city) {
@@ -117,6 +117,7 @@ const getAllProperties = (options, limit = 10) => {
     queryString += ` AND properties.owner_id = $${queryParams.length}`;
   }
   if (options.minimum_price_per_night) {
+    //console.log("minimum", options.minimum_price_per_night);
     queryParams.push(options.minimum_price_per_night * 100); // Convert dollars to cents
     queryString += ` AND cost_per_night >= $${queryParams.length}`;
   }
@@ -128,6 +129,7 @@ const getAllProperties = (options, limit = 10) => {
   queryString += `
     GROUP BY properties.id
   `;
+
   if (options.minimum_rating) {
     queryParams.push(parseInt(options.minimum_rating));
     queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
@@ -137,6 +139,8 @@ const getAllProperties = (options, limit = 10) => {
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
   `;
+  console.log("query string", queryString);
+  console.log("query params", queryParams);
 
   return queryDatabase(queryString, queryParams);
 };
